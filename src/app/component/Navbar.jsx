2d9780a68d29e7784498eb95716d7a8acc2e67e9
saad7528 <saad0174742@gmail.com>
2026-04-27 23:39:28 +0600

    add navbar

---
+++ src/app/component/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link, Button } from "@heroui/react";
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <nav className="sticky top-0 z-40 w-full border-b border-separator bg-background/70 backdrop-blur-lg">
+                <header className="flex h-16 items-center justify-between px-6">
+                    <div className="flex items-center gap-3">
+                        <p className="font-bold">ACME</p>
+                    </div>
+                    <ul className="flex items-center gap-4">
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="/heroui">HeroUi</Link></li>
+                        <li><Link href="/tasks">Tasks</Link></li>
+                    </ul>
+                </header>
+            </nav>
+            {/* With right-aligned content */}
+            {/* <nav className="sticky top-0 z-40 w-full border-b border-separator bg-background/70 backdrop-blur-lg">
+                <header className="flex h-16 items-center justify-between px-6">
+                    <div>Logo</div>
+                    <ul className="flex items-center gap-4">
+                        <li><Button>Sign Up</Button></li>
+                    </ul>
+                </header>
+            </nav> */}
+
+        </div>
+    );
+};
+
+export default Navbar;
--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,9 +1,17 @@
+import { getTasks } from '@/lib/tasks';
 import React from 'react';
+import Task from '../component/Task';
 
-const Tasks = () => {
+const Tasks = async () => {
+    const tasks = await getTasks()
     return (
         <div>
-            <h1>Task</h1>
+            <h1>Task : {tasks.length}</h1>
+            <div className='grid grid-cols-3 gap-4'>
+                {
+                    tasks.map(task => <Task key={task.id} task={task}/>)
+                }
+            </div>
         </div>
     );
 };
