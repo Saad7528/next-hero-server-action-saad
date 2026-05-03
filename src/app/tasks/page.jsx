c2b8695e4e1b54f6c117629557872ba0f3bae7f8
saad7528 <saad0174742@gmail.com>
2026-05-03 23:19:12 +0600

    add action file and modal

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,13 +1,16 @@
 import { getTasks } from '@/lib/tasks';
 import React from 'react';
 import Task from '../component/Task';
+import { AddTask } from '../component/AddTask';
+import { createATask } from '../lib/actions';
 
 const Tasks = async () => {
     const tasks = await getTasks();
     return (
         <div>
-            <h1>Task : {tasks.length}</h1>
-            <div className='grid grid-cols-3 gap-4'>
+            <h1 className='mb-5'>Task : {tasks.length}</h1>
+            <AddTask createATask = {createATask}></AddTask>
+            <div className='grid grid-cols-3 gap-4 mt-5'>
                 {
                     tasks.map(task => <Task key={task.id} task={task}/>)
                 }
